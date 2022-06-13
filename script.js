6e3ec328921d4251482bc0fafeb8af058903b45b
Julia Kupreichik <julilike@yandex.ru>
2022-06-13 15:38:57 +0300

    feat: add three sections

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const mediaQuery = window.matchMedia('(max-width: 690px)');
 const navBarEl = document.querySelector('.nav-bar');
 
-function handleViewWidthChange(e) {
-  if (e.matches) {
+function handleViewWidthChange() {
+  if (window.matchMedia('(max-width: 690px)').matches) {
     navBarEl.style.display = 'block'
   } else {
     navBarEl.style.display = 'none'
   }
 };
 
-mediaQuery.addEventListener('resize', handleViewWidthChange);
-handleViewWidthChange(mediaQuery);
+window.addEventListener('resize', handleViewWidthChange);
+handleViewWidthChange();
 
 const navMenuEl = document.querySelector('.nav-list');
 
